085eccef7b44b07d148e212c64a23300058085e4
IvannaB <ivankamorkai.baglay@gmail.com>
2020-02-17 08:15:09 +0200

    Added thread for send message

--- ibahlai/cmd-chat/Project1/main.cpp
+++ ibahlai/cmd-chat/Project1/main.cpp
@@ -1,6 +1,59 @@
+#include <iostream>
+#include <thread>
+
 #include "Socket.h"
+
+void ThreadSendFunction(Socket& socket, bool& isFinish)
+{
+    std::cout << "Enter (q) to exit" << std::endl;
+    while (!isFinish)
+    {
+        std::string message;
+        std::cout << "You: ";
+        std::getline(std::cin, message);
+        if (message != "q")
+        {
+            socket.Send(message);
+        }
+        else
+        {
+            isFinish = true;
+        }
+    }
+};
+
 
 int main()
 {
-    return 0;
+    Socket socket;
+
+    if (!socket.Init())
+    {
+        std::cout << "Init socket failed" << std::endl;
+        return 1;
+    }
+    if (!socket.Create())
+    {
+        std::cout << "Create socket failed" << std::endl;
+        return 1;
+    }
+    if (!socket.Bind())
+    {
+        std::cout << "Bind failed" << std::endl;
+        return 1;
+    }
+
+    bool isFinish = false;
+
+
+    std::thread sendThread(ThreadSendFunction, socket, isFinish);
+
+    while (!isFinish)
+    {
+        socket.Receive();
+    }
+
+    sendThread.join();
+
+
 }
